d8955190fd242a12f3853e8a41ba052ef33de30c
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-06-27 01:07:48 -0700

    Bubble Sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,27 +11,27 @@
 
 void bubble_sort(int *array, size_t size)
 {
-        size_t i, j, temp, flag;
+	size_t i, j, temp, flag;
 
-        if (!size || !array)
-                return;
+	if (!size || !array)
+		return;
 
-        for (i = 0; i < size - 1; i++)
-        {
-                flag = 0;
-                for (j = 0; j < size - i - 1; j++)
-                {
-                        if (array[j] > array[j + 1])
-                        {
-                                flag = 1;
-                                temp = array[j];
-                                array[j] = array[j + 1];
-                                array[j + 1] = temp;
-                                print_array(array, size);
-                        }
-                }
+	for (i = 0; i < size - 1; i++)
+	{
+		flag = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				flag = 1;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
 
-                if (flag == 0)
-                        break;
-        }
+		if (flag == 0)
+			break;
+	}
 }
