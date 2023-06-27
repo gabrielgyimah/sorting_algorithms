6f01d8b085cec31cf278240368561fd01259311b
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-06-27 13:02:45 -0700

    header file

--- sort.h
+++ sort.h
@@ -27,5 +27,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void lomuto_qsort(int *array, int lo, int hi, size_t size);
 int partition(int *array, int lo, int hi, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif /*SORT_H*/
